721cfffa5deddbb7756ee71ea1a5b3d3c0c35dba
Thorsten Bux <thorsten.bux@outlook.com>
2017-12-21 15:21:08 +1300

    docker support

--- server_data.php
+++ server_data.php
@@ -8,15 +8,15 @@ if (! defined('DEBUG')) {
     define('DEBUG', false);        // Set to true to turn debugging on
 }
 
-$dbIP = '127.0.0.1';               // Database server IP.
+$dbIP = 'mysql';               // Database server IP.
 $dbPort = 3306;                    // Database server port.
-$dbUser = 'USERNAME'               // Database User Name for read access
-$dbPass = 'PASSWORD';              // Database Password for read access
-$dbUserWrite = 'USERNAME';         // Database User Name for write access
-$dbPassWrite = 'PASSWORD';         // Database Password for write access
+$dbUser = 'root';               // Database User Name for read access
+$dbPass = 'rootPw';              // Database Password for read access
+$dbUserWrite = 'root';         // Database User Name for write access
+$dbPassWrite = 'rootPw';         // Database Password for write access
 $dbName = 'calib_camera';          // Database Name
 
 // ENTER YOUR OWN SECRET TEXT STRING BELOW. 
-$serverAuthTokenUpload = ;
+$serverAuthTokenUpload = '';
 $serverAuthTokenDownload = 'ARToolKit.Rocks';
 ?>
